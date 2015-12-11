655aa423de4fd3a4cd237720daf89c0a25f46738
miyazaki <miyazaki_masayasu_f5@lab.ntt.co.jp>
2015-12-11 18:24:26 +0900

    does not work

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,4 @@ import Lib
 
 main :: IO ()
 main = do
-  print $ kleinberg [1,3,4,5,19,29] $ defOpts
+  print $ kleinberg [1,2,3,10,11,12,13,14,20,25,30] $ defOpts
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,30 +2,10 @@ module Lib
     ( kleinberg
      ,KleinbergOpts(state,gamma)
      ,defOpts
-     ,replaceNth
     ) where
 
 import Data.List
 import Data.Matrix
-
-calcGaps :: Num b => [b] -> [b]
-calcGaps offsets =
-    map (\(a,b) -> a - b) $ zip list1 list2
-    where
-      list1 = tail offsets
-      list2 = init offsets
-
--- # Compute an upper bound on the number of states used in the optimal state
--- # sequence, as per Kleinberg.
-computeK
-  :: (Integral r, Integral b, Real a, Real a1, Foldable t) =>
-     a1 -> t r -> a -> b
-computeK state gaps total =
-    ceiling $ 1 + c + b
-    where
-      a = 1 / (fromIntegral $ minimum gaps)
-      b = logBase (realToFrac state) a
-      c = logBase (realToFrac total) (realToFrac state)
 
 data KleinbergOpts = KleinbergOpts {
       state :: Int,
@@ -35,6 +15,38 @@ data KleinbergOpts = KleinbergOpts {
 defOpts :: KleinbergOpts
 defOpts =  KleinbergOpts {state=2, gamma=1}
 
+kleinberg offsets opts =
+    foldl (\x t -> let (cprime,qprime) = x
+                       c = map (\j ->
+                                    let cost = map (\ell ->
+                                                    cprime !! ell +
+                                                    tau (realToFrac ell) (realToFrac j))
+                                               [0..(k - 1)]
+                                        minidx = minIndex cost
+                                    in (cost !! minidx - log (f j $ realToFrac $ gaps !! t), minidx))
+                           [0..(k - 1)]
+                       p = map (\ell -> ell) (map snd c)
+                   in (map fst c,qprime))
+              (initC, initQ)
+              [0..(len - 1)]
+    where s = (state opts)
+          g = (gamma opts)
+          gaps = calcGaps $ sort offsets
+          len = length gaps
+          sumGaps = sum gaps
+          k = ceiling $ 1 +
+              log' (realToFrac sumGaps) (realToFrac s) +
+              log' (1 / (realToFrac $ minimum gaps)) (realToFrac s)
+          tau = let gammalogn = realToFrac g * log (realToFrac len)
+                in \i j -> if i >= j then 0 else (j -i) * gammalogn
+          f = let ghat = fromIntegral sumGaps / (fromIntegral len)
+              in \i x -> let alpha = (realToFrac s) ** (realToFrac i) / ghat
+                         in alpha * realToFrac (exp $ -alpha * x)
+          inf = 1/0
+          na = 0/0
+          initC = 0:(take (k - 1) $ repeat inf)
+          initQ = matrix k sumGaps $ \(i, j) -> na
+
 minIndex :: Ord b => [b] -> Int
 minIndex xs = head $ filter ((== minimum xs) . (xs !!)) [0..]
 
@@ -42,30 +54,12 @@ replaceNth n newVal (x:xs)
     | n ==  0 = newVal:xs
     | otherwise = x:replaceNth (n-1) newVal xs
 
-kleinberg offsets opts =
-    foldl (\x t ->
-               let (cprime, qprime) = x
-               in foldl (\y j ->
-                        let (c, q) = y
-                            cost = map (\ell ->
-                                            c !! ell + tau (realToFrac ell) (realToFrac j)) [0..(k - 1)]
-                            minidx = minIndex cost
-                            cresult = cost !! minidx - (f j (realToFrac (gaps !! t)))
-                        in (replaceNth minidx cresult c, q))
-               (cprime, qprime)
-               [0..(k - 1)])
-    (cdef, qdef)
-    [0..(len - 1)]
-    where gaps = calcGaps $ sort offsets
-          total = sum gaps
-          len = length gaps
-          ghat = (fromIntegral total) / (fromIntegral len)
-          k = computeK (state opts) gaps total
-          gammalogn = realToFrac (gamma opts) * log (realToFrac len)
-          tau = \x y -> if x >= y then 0 else (y -x) * gammalogn
-          alpha = map (\x -> realToFrac (state opts) ** (realToFrac x) / ghat) [0..(k - 1)]
-          f = \j x -> (alpha !! j) * (exp $ -1 * (alpha !! j) * x)
-          inf = 1/0
-          na = 0/0
-          cdef = 0:(take (k - 1) $ repeat inf)
-          qdef = matrix k total $ \(i,j) -> na
+calcGaps :: Num b => [b] -> [b]
+calcGaps offsets =
+    map (\(a,b) -> a - b) $ zip list1 list2
+    where
+      list1 = tail offsets
+      list2 = init offsets
+
+log' :: Floating a => a -> a -> a
+log' val base = log val / log base
