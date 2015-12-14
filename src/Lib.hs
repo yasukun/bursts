ea93f3c118e643fc7c8779b276b3bdf61bb220c8
miyazaki <miyazaki_masayasu_f5@lab.ntt.co.jp>
2015-12-14 14:49:45 +0900

    does not work

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,7 +5,6 @@ module Lib
     ) where
 
 import Data.List
-import Data.Matrix
 
 data KleinbergOpts = KleinbergOpts {
       state :: Int,
@@ -15,27 +14,59 @@ data KleinbergOpts = KleinbergOpts {
 defOpts :: KleinbergOpts
 defOpts =  KleinbergOpts {state=2, gamma=1}
 
+minIndex :: Ord b => [b] -> Int
+minIndex xs = head $ filter ((== minimum xs) . (xs !!)) [0..]
+
+replaceNth :: (Eq a, Num a) => a -> a1 -> [a1] -> [a1]
+replaceNth n newVal (x:xs)
+    | n ==  0 = newVal:xs
+    | otherwise = x:replaceNth (n-1) newVal xs
+
+calcGaps :: Num b => [b] -> [b]
+calcGaps offsets =
+    map (\(a,b) -> a - b) $ zip list1 list2
+    where
+      list1 = tail offsets
+      list2 = init offsets
+
+log' :: Floating a => a -> a -> a
+log' val base = log val / log base
+
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = take (to - from + 1) (drop from xs)
+
+computeOpt
+  :: (Floating b, Fractional b1, Fractional b2, Fractional r1, Ord b,
+      Real r) =>
+     (Int -> r1 -> b)
+     -> (b1 -> b2 -> b)
+     -> Int
+     -> [r]
+     -> [b]
+     -> [[Int]]
+     -> [Int]
+     -> ([b], [[Int]])
+computeOpt _ _ _ _ caccum paccum [] = (caccum, paccum)
+computeOpt f tau k gaps caccum paccum (t:ts) =
+    computeOpt f tau k gaps cprime pprime ts
+    where
+      computeOpt' paths [] = []
+      computeOpt' paths (j:js) =
+          ret:computeOpt' path js
+          where
+            cost = map (\ell -> caccum !! ell + tau (realToFrac ell) (realToFrac j)
+                       - log (f j $ realToFrac $ gaps  !!  t)) [0..(k - 1)]
+            minidx = minIndex cost
+            cprime' = cost !! minidx
+            path = replaceNth j (slice 0 (t + 1) paths !! minidx ++ [j]) paths
+            ret = (cprime',path)
+      result = computeOpt' paccum [0..(k - 1)]
+      pprime = snd $ last result
+      cprime = map fst result
+
+kleinberg :: Integral b => [b] -> KleinbergOpts -> [Int]
 kleinberg offsets opts =
-    foldl (\x t -> let (cprime,qprime) = x
-                       c = map (\j ->
-                                    let cost = map (\ell ->
-                                                    cprime !! ell +
-                                                    tau (realToFrac ell) (realToFrac j))
-                                               [0..(k - 1)]
-                                        minidx = minIndex cost
-                                    in (cost !! minidx - log (f j $ realToFrac $ gaps !! t), minidx))
-                           [0..(k - 1)]
-                       elems = (map snd c)
-                       q = foldl (\q' y ->
-                                      let (ell, j) = y
-                                          newval = realToFrac j
-                                      in setElem  newval ((j - 1), (t - 1)) q'
-                                 )
-                           qprime
-                           (zip elems [0..(length elems  - 1)])
-                   in (map fst c,qprime))
-              (initC, initQ)
-              [0..(len - 1)]
+    paths !! (minIndex finalcost)
     where s = (state opts)
           g = (gamma opts)
           gaps = calcGaps $ sort offsets
@@ -52,21 +83,5 @@ kleinberg offsets opts =
           inf = 1/0
           na = 0/0
           initC = 0:(take (k - 1) $ repeat inf)
-          initQ = matrix k sumGaps $ \(i, j) -> na
-
-minIndex :: Ord b => [b] -> Int
-minIndex xs = head $ filter ((== minimum xs) . (xs !!)) [0..]
-
-replaceNth n newVal (x:xs)
-    | n ==  0 = newVal:xs
-    | otherwise = x:replaceNth (n-1) newVal xs
-
-calcGaps :: Num b => [b] -> [b]
-calcGaps offsets =
-    map (\(a,b) -> a - b) $ zip list1 list2
-    where
-      list1 = tail offsets
-      list2 = init offsets
-
-log' :: Floating a => a -> a -> a
-log' val base = log val / log base
+          initPath = take k $ repeat [0]
+          (finalcost, paths) = computeOpt f tau k gaps initC initPath [0..(len - 1)]
